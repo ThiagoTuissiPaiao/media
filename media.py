b55563a4ff37024a99711f36bb28f0dfda1930cb
Thiago Tuissi Paião <81596921+ThiagoTuissiPaiao@users.noreply.github.com>
2021-04-12 23:10:15 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,10 @@
+# Colete a primera nota.
+nota1 = float(input("Digite a PRIMERA nota: "))
+# Colete a segunda nota.
+nota2 = float(input("Digite a SEGUNDA nota: "))
+
+# Coleo a segunda nota.
+media = (nota1 + nota2) / 2
+
+# Escreva na tela a media
+print(f"Sua media final é {media:.2}")
